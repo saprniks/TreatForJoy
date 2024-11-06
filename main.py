e31363129c817a1c12864ea6f34996ceb9a235d7
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-11-06 01:56:25 +0000

    webhooks introduced

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, Update
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
+from aiohttp import web
 import asyncio
 import os
+
 from dotenv import load_dotenv
 
 # Загружаем переменные окружения из .env
 load_dotenv()
 
 API_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')  # URL, куда Telegram будет отправлять обновления
 
 # Инициализируем бота и диспетчер
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
-
 
 # Обработчик команды /start
 @dp.message(Command("start"))
 async def send_welcome(message: Message):
     await message.answer("Привет! Я фото бот.")
 
+# Основная асинхронная функция для запуска бота через вебхук
+async def on_startup(app: web.Application):
+    # Устанавливаем вебхук
+    await bot.set_webhook(WEBHOOK_URL)
 
-# Основная асинхронная функция для запуска бота
-async def main():
-    # Запуск поллинга
-    await dp.start_polling(bot)
+async def on_shutdown(app: web.Application):
+    # Убираем вебхук при завершении работы
+    await bot.delete_webhook()
+
+# Настройка веб-сервера для приема обновлений
+app = web.Application()
+app.on_startup.append(on_startup)
+app.on_shutdown.append(on_shutdown)
+SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="/webhook")
 
 if __name__ == "__main__":
-    # Запуск асинхронного приложения
-    asyncio.run(main())
+    # Запуск веб-сервера
+    web.run_app(app, port=8000)
