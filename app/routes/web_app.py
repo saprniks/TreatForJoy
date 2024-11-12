63c2705b21a2b6ae7e65121608064c1dc80f356a
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-11-12 03:15:59 +0000

    2nd try

--- app/routes/web_app.py
+++ app/routes/web_app.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/catalog")
+@router.get("/")
 async def get_catalog(request: Request):
     # Здесь пока что простой вывод "Hello world"
     return templates.TemplateResponse("index.html", {"request": request})
