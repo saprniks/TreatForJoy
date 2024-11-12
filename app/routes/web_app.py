b8c1a412ca9a397ea5dd325faf2cebf80eaf6107
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-11-12 14:48:01 +0000

    пробуем получить данные из бд

--- app/routes/web_app.py
+++ app/routes/web_app.py
@@ -1,11 +1,20 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from app.models.models import Album
+from app.utils.db import get_db
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/")
-async def get_catalog(request: Request):
-    # Здесь пока что простой вывод "Hello world"
-    return templates.TemplateResponse("index.html", {"request": request})
+
+@router.get("/", response_class=HTMLResponse)
+async def get_catalog(request: Request, db: AsyncSession = Depends(get_db)):
+    # Запрашиваем все альбомы из базы данных
+    result = await db.execute(select(Album))
+    albums = result.scalars().all()
+
+    # Передаем список альбомов в шаблон
+    return templates.TemplateResponse("index.html", {"request": request, "albums": albums})
