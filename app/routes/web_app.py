63cf699c214b1b6b95bb9308307af79a6d149638
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-11-13 13:35:12 +0000

    карточки без фото

--- app/routes/web_app.py
+++ app/routes/web_app.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from app.crud import album_crud, user_crud
+from app.crud import album_crud, user_crud, item_crud
 from app.utils.db import get_db
 from fastapi.templating import Jinja2Templates
 import logging
@@ -13,11 +13,15 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def get_catalog(request: Request, db: AsyncSession = Depends(get_db)):
-    # Запрашиваем все альбомы из базы данных
+    # Запрашиваем все альбомы и изделия из базы данных
     albums = await album_crud.get_all_albums(db)
+    items_all = await item_crud.get_all_items(db)
 
-    # Передаем список альбомов в шаблон
-    return templates.TemplateResponse("index.html", {"request": request, "albums": albums})
+    # Отбираем изделия с display_order <= 3
+    filtered_items = [item for item in items_all if item.display_order <= 3]
+
+    # Передаем список альбомов и отфильтрованные изделия в шаблон
+    return templates.TemplateResponse("index.html", {"request": request, "albums": albums, "items": filtered_items})
 
 
 @router.post("/register_user")
